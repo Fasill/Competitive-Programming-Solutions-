4ea5e19766fdfd8e802686859a3a9002eabe7d65
FASIL <81506454+Fasill@users.noreply.github.com>
2024-01-01 13:41:04 +0300

    Added solution for Reduction Operations to Make the Array Elements Equal

--- python track/reduction-operations-to-make-the-array-elements-equal.py
+++ python track/reduction-operations-to-make-the-array-elements-equal.py
@@ -1,22 +1,13 @@
 
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
-
+        # time complexity O(nlogn)
         def summationn(n):
             return (n*(n+1))//2
-            
-        def sequenceSum(n):
-            n = n-1
-            if n % 2 != 0:
-                l = (n // 2) + 1
-                return (l / 2) * ((2 * n) +( (l - 1) * 2))
-            else:
-                l = n / 2
-                return (l / 2) * ((2 * n) + (l - 1) * 2)
-
 
         if len(set(nums)) == len(nums):
             return summationn(len(nums)-1)
+
         elif  len(set(nums)) > 1:
             d = Counter(nums)
            
@@ -28,9 +19,6 @@ class Solution:
                 s+=(d[i]*c)
                 c-=1
             return s
-
-
-
 
         else: return 0
 
