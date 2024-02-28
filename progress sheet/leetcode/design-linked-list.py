9c4bde3e3ea7481e880d23dd4e035e0f8dc0140e
FASIL <81506454+Fasill@users.noreply.github.com>
2024-02-28 21:24:10 +0300

    Add solution for Design Linked List

---
+++ progress sheet/leetcode/design-linked-list.py
@@ -0,0 +1,72 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.size:
+            return -1
+        cur = self.head
+        for _ in range(index):
+            cur = cur.next
+        return cur.val
+
+    def addAtHead(self, val: int) -> None:
+        new_node = ListNode(val)
+        new_node.next = self.head
+        self.head = new_node
+        self.size += 1
+
+    def addAtTail(self, val: int) -> None:
+        new_node = ListNode(val)
+        if not self.head:
+            self.head = new_node
+        else:
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = new_node
+        self.size += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0 or index > self.size:
+            return
+        if index == 0:
+            self.addAtHead(val)
+        elif index == self.size:
+            self.addAtTail(val)
+        else:
+            new_node = ListNode(val)
+            cur = self.head
+            for _ in range(index - 1):
+                cur = cur.next
+            new_node.next = cur.next
+            cur.next = new_node
+            self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size:
+            return
+        if index == 0:
+            self.head = self.head.next
+        else:
+            cur = self.head
+            for _ in range(index - 1):
+                cur = cur.next
+            cur.next = cur.next.next
+        self.size -= 1
+
+# Test the implementation
+# obj = MyLinkedList()
+# obj.addAtHead(1)
+# obj.addAtTail(3)
+# obj.addAtIndex(1, 2)
+# print(obj.get(1))  # Output: 2
+# obj.deleteAtIndex(1)
+# print(obj.get(1))  # Output: 3
