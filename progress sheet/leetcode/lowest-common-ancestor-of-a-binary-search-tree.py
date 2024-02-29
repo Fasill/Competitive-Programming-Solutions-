f3e695c296a96dd0a607c4fac561a4c75c8df232
FASIL <81506454+Fasill@users.noreply.github.com>
2024-02-29 22:01:53 +0300

    Add solution for Lowest Common Ancestor of a Binary Search Tree

--- progress sheet/leetcode/lowest-common-ancestor-of-a-binary-search-tree.py
+++ progress sheet/leetcode/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -6,27 +6,33 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        li = []
-        def ancestor(node1,p,q):
-            if p.val == node1.val:
-                li.append(p)
-                return
-            if q.val == node1.val:
-                li.append(q)
-                return
-            if  p.val < node1.val and q.val > node1.val:
-                li.append( node1)
-                
-                return 
-            li.append( node1)
-            
-            if p.val < node1.val and q.val < node1.val:
-                return ancestor(node1.left,p,q)
-                            
-            if p.val > node1.val and q.val > node1.val:
-                return ancestor(node1.right,p,q)
+    def search(self,node,val):
+        stack = []
+        def dfs(node,val):
 
-        ancestor(root,p,q)
-        return li[-1]
-        
+            if not node:
+                return
+                
+            stack.append(node)
+            if node.val == val:
+                return stack
+
+            left =  dfs(node.left,val)
+
+            if left:
+                return left
+
+            right = dfs(node.right,val)
+            if right:
+                return right
+
+            stack.pop()
+            return None
+        return dfs(node,val)
+
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        first = set(self.search(root,p.val))
+        for i in self.search(root,q.val)[::-1]:
+            if i in first:
+                return i
