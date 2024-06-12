a632bb99af38b3c08808fb211786ebd35e470a47
FASIL <81506454+Fasill@users.noreply.github.com>
2024-06-12 11:14:26 +0300

    Created 'Long Pressed Name' by A2SV Hub

---
+++ 12-Jun-2024/Long Pressed Name 166990.py
@@ -0,0 +1,12 @@
+# Problem: Long Pressed Name - https://leetcode.com/problems/long-pressed-name/
+
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        i, j = 0, 0
+        while j < len(typed):
+            if i < len(name) and name[i] == typed[j]:
+                i += 1
+            elif j == 0 or typed[j] != typed[j - 1]:
+                return False
+            j += 1
+        return i == len(name)
