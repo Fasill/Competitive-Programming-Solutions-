816630ac50d788ca7ac3aaab83c88831006dbf3a
FASIL <81506454+Fasill@users.noreply.github.com>
2024-07-02 21:39:12 +0300

    Created 'B - Distinct Digits' by A2SV Hub

---
+++ A2SV G5 - Contest #21 02-Jul-2024/B - Distinct Digits 175700.py
@@ -0,0 +1,18 @@
+# Problem: B - Distinct Digits - https://codeforces.com/gym/530187/problem/B
+
+
+t = int(input())
+for _ in range(t):
+    n = int(input())   
+    
+    def dp(i,tot):
+        if tot == n:
+            return str(i)
+        ans = "999999999"
+        for j in range(i + 1,10):
+            if tot + j <= n:
+                ans = min(int(ans),int(str(i) + dp(j,tot + j)))            
+            
+        return str(ans)
+    
+    print(dp(0,0))
