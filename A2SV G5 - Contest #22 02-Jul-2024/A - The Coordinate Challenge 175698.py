8c41fb301daea24139be5bfe362dce4ec250de74
FASIL <81506454+Fasill@users.noreply.github.com>
2024-07-02 21:38:33 +0300

    Created 'A - The Coordinate Challenge' by A2SV Hub

---
+++ A2SV G5 - Contest #22 02-Jul-2024/A - The Coordinate Challenge 175698.py
@@ -0,0 +1,9 @@
+# Problem: A - The Coordinate Challenge - https://codeforces.com/gym/531455/problem/A
+
+t = int(input())
+for _ in range(t):
+    num = int(input()) 
+    if num == 1:
+        print(2)
+    else:
+        print((num+2) //3)
