ef67b0d4e9cd120fe61a51d0704ff4509ffd5d59
FASIL <81506454+Fasill@users.noreply.github.com>
2024-07-02 21:39:27 +0300

    Created 'A - Spris' by A2SV Hub

---
+++ A2SV G5 - Contest #20 02-Jul-2024/A - Spris 175702.py
@@ -0,0 +1,17 @@
+# Problem: A - Spris - https://codeforces.com/gym/528792/problem/A
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a <= 0:
+    print(0)
+else:
+    _min= float("inf")
+    _min = min(b// 2 ,_min)
+    _min = min(c// 4 ,_min)
+    _min = min(a,_min)
+    print((_min * 1)+(_min * 2) + (_min * 4))
+    
+    
+    
