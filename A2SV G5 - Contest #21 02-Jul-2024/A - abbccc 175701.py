c2c9540f3a26596335b814a32ad33cc010a763db
FASIL <81506454+Fasill@users.noreply.github.com>
2024-07-02 21:39:19 +0300

    Created 'A - abbccc' by A2SV Hub

---
+++ A2SV G5 - Contest #21 02-Jul-2024/A - abbccc 175701.py
@@ -0,0 +1,14 @@
+# Problem: A - abbccc - https://codeforces.com/gym/530187/problem/A
+
+t = int(input())
+s = input()
+
+res = []
+i = 0
+c = 0
+while i < t:
+    res.append(s[i])
+    
+    c += 1
+    i += c
+print("".join(res))
