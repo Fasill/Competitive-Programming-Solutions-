3bba5ef328a8bae517178a79c60675966bcf2614
FASIL <81506454+Fasill@users.noreply.github.com>
2024-07-06 21:46:30 +0300

    Created 'Vertical Order Traversal of a Binary Tree' by A2SV Hub

---
+++ 06-Jul-2024/Vertical Order Traversal of a Binary Tree 176920.py
@@ -0,0 +1,31 @@
+# Problem: Vertical Order Traversal of a Binary Tree - https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        li = []
+        def inorder(node,i):
+            if node:
+                li.append((i[0],i[-1],node.val))
+                inorder(node.left,(i[0]+1,i[-1]-1))
+                inorder(node.right,(i[0]+1,i[-1]+1))
+                
+        inorder(root,(0,0))
+        sortedByCol = sorted(li,key = lambda x:(x[1],x[0],x[2]))
+
+        d = defaultdict(list)
+        for i in sortedByCol:
+            d[i[1]].append(i[-1])
+
+
+        res = []
+        for i in d:
+            res.append(d[i])
+            
+        return res
+
