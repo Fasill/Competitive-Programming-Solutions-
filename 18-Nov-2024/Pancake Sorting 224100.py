a1d50874321c4bcd42f59ab12c3a815527768d71
FASIL <81506454+Fasill@users.noreply.github.com>
2024-11-18 18:32:17 +0300

    Created 'Pancake Sorting' by A2SV Hub

---
+++ 18-Nov-2024/Pancake Sorting 224100.py
@@ -0,0 +1,25 @@
+# Problem: Pancake Sorting - https://leetcode.com/problems/pancake-sorting/
+
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+
+        l,r = arr.index(max(arr))+1,len(arr)-1
+        ans = []
+        while 0<=r:
+            if arr[:r+1]:
+                l = arr.index(max(arr[:r+1]))
+                if l == r:
+                    r-=1
+                    print("s")
+                else:
+                    arr[:l+1] = arr[:l+1][::-1]
+                    ans.append(l+1)
+                    arr[:r+1] = arr[:r+1][::-1]
+                    ans.append(r+1)
+                    r-=1
+
+            else:r-=1
+        return ans
+
+            
+
